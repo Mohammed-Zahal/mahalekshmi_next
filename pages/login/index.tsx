4f16f32d5151b059ea9a92075aa827eac6673712
zahal <muhdzahal123@gmail.com>
2022-06-09 11:52:01 +0530

    next_mahalekshmi

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,11 +1,9 @@
-import React from 'react'
+import React from "react";
 
-const  Login = () => {
-  return (
-    <div />
-  )
-}
+const Login = () => {
+  return <div />;
+};
 
-export default Login
+export default Login;
 
 // eslint eslint-config-airbnb eslint-config-next eslint-config-node eslint-config-prettier  eslint-config-react-app  eslint-plugin-flowtype eslint-plugin-import eslint-plugin-jsx-a11y  eslint-plugin-node eslint-plugin-prettier  eslint-plugin-react eslint-plugin-react-hooks
