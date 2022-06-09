2e9aef5d094d63d94510a4f77769f90c64398a16
sachidev <sachdevsajeev1044@gmail.com>
2022-06-09 10:26:44 +0530

    initialise project

---
+++ pages/login/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const  Login = () => {
+  return (
+    <div />
+  )
+}
+
+export default Login
+
+// eslint eslint-config-airbnb eslint-config-next eslint-config-node eslint-config-prettier  eslint-config-react-app  eslint-plugin-flowtype eslint-plugin-import eslint-plugin-jsx-a11y  eslint-plugin-node eslint-plugin-prettier  eslint-plugin-react eslint-plugin-react-hooks
